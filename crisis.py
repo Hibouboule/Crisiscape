d0bd55da15b7a36d1717eeb0b4a4694df80c2db5
Hibouboule <sammychapeau@gmail.com>
2026-01-22 19:44:24 +0100

    Add files via upload

---
+++ crisis.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python
+
+#First of all, listen. I can code, but idk how to code. Now, you can read the code :3
+
+from tkinter import *
+import random
+
+#Function called whenever question is selected, and at the beginning :)
+def randomize_bg():
+    global ran_color
+    #the hell is this built-in function
+    ran_color=hex(random.randint(0, 4194303))
+    win.configure(bg="#"+ran_color[2:].zfill(6))
+    question_lbl.config(bg="#"+ran_color[2:].zfill(6))
+    validate_lbl.config(bg="#"+ran_color[2:].zfill(6))
+    button_frm.config(bg="#"+ran_color[2:].zfill(6))
+    answer_frm.config(bg="#"+ran_color[2:].zfill(6))
+    validate_btn.config(highlightbackground="#"+ran_color[2:].zfill(6))
+
+#Validate the answer to the question
+def validate():
+    global current_num
+    global current_str
+    if answer_ent.get() == question[current_num][1]:
+        question[current_num][2]=True
+        validate_str.set("Correcte")
+        validate_lbl.config(fg="darkgreen")
+        validate_btn.config(state="disabled")
+        question_btn[current_num].config(bg="#00ff00")
+    else:
+        validate_str.set("Faux")
+        validate_lbl.config(fg="red")
+
+#Change the current question to the selected one
+def switch(num):
+    global current_str
+    global current_num
+    current_str.set(question[num][0])
+    current_num = num
+    validate_str.set("")
+    answer_ent.delete(0, END)
+    randomize_bg()
+    #Enable or disable the validate button
+    if question[current_num][2] == True:
+        validate_btn.config(state="disabled")
+    else:
+        validate_btn.config(state="active")
+
+ran_color=hex(random.randint(0, 16777215))
+
+question = [
+    #["Question", "Answer", "Is completed"]
+    ["Example 1.", "Réponse 1", False],
+    ["Example 2.", "Réponse 2", False],
+    ["Example 3.", "Réponse 3", False],
+    ["Example 4.", "Réponse 4", False],
+    ["Example 5.", "Réponse 5", False],
+    ["Example 6.", "Réponse 6", False],
+    ["Example 7.", "Réponse 7", False],
+    ["Example 8.", "Réponse 8", False],
+    ["Example 9.", "Réponse 9", False]
+]
+
+question_btn = [None] * len(question)
+
+#Create the windows
+win = Tk()
+#Make the win fullscreen
+win.attributes("-fullscreen", True)
+
+#Create the window that will contain the progress bar
+progress = Tk()
+progress.configure(bg="black")
+
+#The current selected question
+current_num = 0
+current_str = StringVar(win, question[0][0])
+
+valid_img = PhotoImage(file="valid1.png")
+
+#The Correcte or Faux string
+validate_str = StringVar(win, "")
+
+#Container for all the question selection button
+button_frm = Frame(
+    win,
+    bg="#"+ran_color[2:].zfill(6)
+)
+button_frm.pack(side=BOTTOM)
+
+#Element that shows the question
+question_lbl = Label(
+    win,
+    textvariable=current_str,
+    bg="#"+ran_color[2:].zfill(6),
+    font=("Helvetica, 30 bold"),
+    anchor="center"
+)
+question_lbl.pack(
+    side=TOP,
+    pady=(200, 0)
+)
+
+#Element that shows Correcte or Faux
+validate_lbl = Label(
+    win,
+    textvariable=validate_str,
+    bg="#"+ran_color[2:].zfill(6),
+    font=("Arial, 22 italic")
+)
+validate_lbl.pack(
+    side=TOP,
+    pady=30
+)
+
+#Container for the entry and the validate button
+answer_frm = Frame(
+    win,
+    bg="#"+ran_color[2:].zfill(6)
+)
+answer_frm.pack(side=TOP)
+
+#You can enter the answer here
+answer_ent = Entry(
+    answer_frm,
+    font=("Arial, 22"),
+    justify=CENTER,
+    fg="white",
+    bg="black",
+    highlightthickness=0,
+    borderwidth=5
+)
+answer_ent.pack(
+    side=LEFT,
+    padx=(40, 0)
+)
+
+#Confirm the answer
+validate_btn = Button(
+    answer_frm,
+    #text="Valider",
+    image=valid_img,
+    font=("Arial, 26"),
+    relief="ridge",
+    borderwidth=7,
+    bg="darkred",
+    activebackground="red",
+    highlightbackground="#"+ran_color[2:].zfill(6),
+    command=validate,
+    state="active"
+)
+validate_btn.pack(
+    side=RIGHT,
+    pady=20,
+    padx=(10, 20)
+)
+
+#Generate question selection button
+for i in range(len(question)):
+    question_btn[i] = Button(
+        button_frm,
+        text=str(i+1),
+        font=("Arial, 25 bold"),
+        bg="darkorange",
+        activebackground="yellow",
+        command=lambda i=i: switch(i),
+        width=3,
+        borderwidth=5,
+        highlightbackground="black",
+        highlightthickness=3
+    )
+    question_btn[i].pack(
+        side=LEFT,
+        padx=20,
+        pady=(0, 50)
+    )
+
+randomize_bg()
+
+#The mainloop idk
+win.mainloop()
+progress.mainloop()
