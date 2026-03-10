d83fc8997ff6a513f96795bf3c38db63dcd0f0fd
Krochire <marco02cheri@gmail.com>
2026-03-10 11:13:21 +0100

    Reworked adding a question logic; removed leftover debugging statements

--- question_maker.py
+++ question_maker.py
@@ -15,13 +15,11 @@ def main():
         if choice == "save":
             break
         elif choice == "1":
-            json_object.append(add_question(len(json_object)))
+            add_question(json_object)
         elif choice == "2":
             edit_question(json_object)
         elif choice == "3":
             remove_question(json_object)
-            print("Out func: ")
-            print(json_object)
         else:
             print("Mauvaise entrée")
     with open("questions.json", "w", encoding="utf-8") as file:
@@ -34,7 +32,7 @@ def get_choice():
     3. Supprimer une question
 Numéro de l'opération>>>''')
 
-def add_question(question_count):
+def add_question(json_object):
     question_str = input('''Merci d'entrer le texte de votre question.
 Respectez la casse et la ponctuation que vous voulez.
 Pas besoin de mettre de guillemets, mettez juste votre texte exact.
@@ -42,16 +40,17 @@ Pas trop long!!! Une phrase au mieux.
 >>>''')
     answer = input("Merci de faire la même chose avec votre réponse.\n>>>")
     result = input("Merci de faire la même chose avec la phrase donnée après une bonne réponse.\n>>>")
-    question_id = question_count
+    question_id = len(json_object)
 
     print('''Merci, on a bien pris en compte votre question!
 ===============================================''')
-    return {
+    new_question = {
         "question_str": question_str,
         "answer": answer,
         "id": question_id,
         "result": result
         }
+    json_object.append(new_question)
 
 def edit_question(json_object):
     list_questions(json_object)
@@ -92,8 +91,6 @@ def remove_question(json_object):
     json_object.pop(question_choice)
     for question_num in range(len(json_object)):
         json_object[question_num]["id"] = question_num
-    print("In func: ")
-    print(json_object)
 
 def list_questions(json_object):
     for question in json_object:
